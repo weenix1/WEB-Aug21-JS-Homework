b2d995bcc2eb473ef3923cd53156a1eeb789b307
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-09-13 16:29:08 +0200

    exercise 12

--- index.js
+++ index.js
@@ -10,32 +10,108 @@ function intSum(n1, n2) {
 console.log(intSum(2, 2));
 
 /* 2. Create a function to check two given integers. Return `true` if one of them is 50 or if their sum is 50. */
-function checkInt() {}
-
+function checkInt(n1, n2) {
+  let sum = 50;
+  if (n1 === sum || n1 + n2 === sum) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkInt(20, 40));
 /* 3. Create a function to remove a character at a specified position from a given string: pass the position and the string as parameters, return the new string. */
 
+function removeChar(str, char_pos) {
+  let string1 = str.substring(0, char_pos);
+  let string2 = str.substring(char_pos + 1, str.length);
+  const newStr = string1 + string2;
+  return newStr;
+}
+console.log(removeChar("franklin", 5));
 /* 4. Create a function to find and return the largest of three given integers. */
+
+function findLargeInt(n1, n2, n3) {
+  if (n1 > n2 && n2 > n3) {
+    return n1;
+  } else if (n2 > n1 && n1 > n3) {
+    return n2;
+  } else if (n3 > n2 && n2 > n1) {
+    return n3;
+  }
+}
+console.log(findLargeInt(2, 4, 6));
 
 /* 5. Create a function to check if two numbers are in the range 40-60 or 70-100. 
     Return `true` if they do, return `false` if one (or both) don't. */
 
+function checkNumRange(n1, n2) {
+  if (
+    (n1 >= 40 && n1 <= 60 && n2 >= 40 && n2 <= 60) ||
+    (n1 >= 70 && n1 <= 100 && n2 >= 70 && n2 <= 100)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkNumRange(44, 50));
+console.log(checkNumRange(30, 50));
+console.log(checkNumRange(80, 90));
+console.log(checkNumRange(80, 120));
 /* 6. Create a function to create a new string composed of a specified number of copies of a given string. 
     Pass the string and the number of copies as parameters. */
 
+function createString(str, copy_num) {
+  let newStr = str.repeat(copy_num);
+  return newStr;
+}
+console.log(createString("frank ", 3));
+
 /* 7. Create a function to display the city name if the string begins with "Los" or "New". 
     Pass the city name as a parameter. Return `false` if they start with a different string. */
+function displayCityName(cityname) {
+  if (
+    (cityname.length >= 3 && cityname.substring(0, 3) === "Los") ||
+    cityname.substring(0, 3) === "New"
+  ) {
+    return cityname;
+  } else {
+    return false;
+  }
+}
+console.log(displayCityName("main york"));
 
 /* 8. Create a function to calculate and return the sum of all elements from an array with 3 elements. 
     Pass the array as a parameter. */
-
+console.log("_____________________");
+function calcSumArray(array) {
+  let sum = array;
+  for (let i = 0; i < array.length; i++) {
+    sum = array[i];
+  }
+  return sum;
+}
+console.log(calcSumArray(2 + 3 + 4));
 /* 9. Create a function to test if an array of lenght 2 contains 1 OR 3. 
     Return `true` is it does, `false` if it doesn't. */
-
+function testArray(array = [1, 3]) {
+  if (
+    (array.length[0] === 1 || array.length[0] === 3) &&
+    (array.length[1] === 1 || array.length[1] === 3)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(testArray());
 /* 10. Create a function to test if an array of lenght 2 DOES NOT contain 1 or 3. 
     Return `true` if it doesn't, `false` if it does. */
 
 /* 11. Create a function to find the longest string from a given array of strings. 
     Pass the array as parameter and return the longest string. */
+function findLongestStr() {}
 
 /* 12. Create a function to find the types of a given angle:
   1. Acute angle ⇒ between 0 and 90 degrees. Return `acute`.
